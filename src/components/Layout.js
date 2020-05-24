6c700e806e361ea92dee4727aad31e6173ca6975
dd <dqrd123@gmail.com>
2020-05-24 23:52:04 +0900

    feat: 페이지 구현

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Header from "./Header";
-
 const Layout = ({ children }) => (
   <>
     <Header/>
     <div className="bg-gray-200 h-screen">
-      <div className="flex justify-center items-center app-container">
+      <div className="flex justify-center items-center flex-col app-container md:py-10 lg:py-10 relative">
         {children}
       </div>
     </div>
   </>
 );
-
 export default Layout;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,61 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "../assets/service/css/search.css";
+import InputStation from '../components/InputStation';
+import PathResult from '../components/PathResult';
+import axios from 'axios';
 
-const Search = props => (
-  <>
-    <div className="max-w-sm w-full lg:width-350px rounded bg-white shadow-lg px-6 pt-6 pb-2">
-      <div className="font-bold text-xl mb-4 text-center">지하철 경로 검색</div>
-      <form className="bg-white mb-4">
-        <div className="flex flex-wrap mb-3">
-          <div className="w-5/12 h-12 text-center text-gray-800">
-            <input
-              className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
-              id="departure-station-name"
-              type="text"
-              value=""
-              placeholder="출발역"
-            />
-          </div>
-          <div className="w-2/12 h-12 text-center text-gray-800 flex justify-center items-center">
-            <span className="mdi mdi-arrow-right-thick relative bottom-6px text-lg"></span>
-          </div>
-          <div className="w-5/12 h-12 text-center text-gray-800">
-            <input
-              className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
-              id="arrival-station-name"
-              type="text"
-              value=""
-              placeholder="도착역"
-            />
-          </div>
-        </div>
-        <button type="submit" id="search-button"
-          className="w-full text-sm bg-yellow-500 hover:bg-yellow-400 hover:text-gray-700 text-gray-800 font-bold py-2 px-4 rounded-sm">
-          검색
-        </button>
-      </form>
-    </div>
-    <div id="search-result-container"
-      className="max-w-sm w-full lg:width-350px rounded bg-white shadow-lg p-6 relative border top hidden">
-      <button
-        id="favorite-button"
-        className="favorite-button bg-yellow-500 mdi mdi-star-outline absolute text-white bg-transparent d-inline-block font-semibold py-2 px-3 hover:border-transparent rounded-full z-10"
-      >
-      </button>
-      <ul className="flex border-b w-full">
-        <li id="shortest-distance-tab" className="-mb-px w-2/4 active-tab tab-menu">
-          <a className="w-full text-center inline-block py-2 px-4 font-semibold" href="#">최단거리</a>
-        </li>
-        <li id="minimum-time-tab" className="-mb-px w-2/4 tab-menu">
-          <a className="w-full text-center bg-white inline-block py-2 px-4 font-semibold" href="#"
-          >최소시간</a
-          >
-        </li>
-      </ul>
-      <div id="search-result"></div>
-    </div>
-  </>
-);
+
+const Search = ({ location }) => {
+  const [showResult, setShowResult] = useState("hidden");
+  const [data, setData] = useState(null);
+  const [url, setUrl] = useState("/search");
+  const [type, setType] = useState("DISTANCE");
+
+  const searchPath = (source, target, type) => {
+    const url = "/paths?source=" + source + "&target=" + target + "&type=" + type;
+    setUrl(url);
+  }
+
+  const toggleHidden = () => {
+    setShowResult("");
+  }
+
+  const alterType = (e) => {
+    if (e.target.id === "distance") {
+      setType("DISTANCE");
+    } else {
+      setType("DURATION");
+    }
+  }
+
+  useEffect(() => {
+    if (url && url !== "/search") {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(url);
+          setData(response.data);
+          console.log(response.data);
+        }
+        catch (e) {
+          console.log(e);
+        }
+      };
+      fetchData();
+    }
+  }, [url]);
+
+  useEffect(() => {
+    return () => {
+      setShowResult("hidden");
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="max-w-sm w-full lg:width-350px rounded bg-white shadow-lg px-6 pt-6 pb-2">
+        <div className="font-bold text-xl mb-4 text-center">지하철 경로 검색</div>
+        <InputStation toggleHidden={toggleHidden} url={url} searchPath={searchPath} type={type}/>
+      </div>
+      <PathResult showResult={showResult} data={data} type={type} alterType={alterType}/>
+    </>
+  );
+};
 
 export default Search;
