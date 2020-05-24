856190bbff3b6ccf8ae1a3e7e7c344c346d34dd6
dd <dqrd123@gmail.com>
2020-05-24 23:52:18 +0900

    feat: 경로 찾기 구현

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Home from "./pages/Home";
 import Layout from "./components/Layout";
 
 import "./assets/service/css/app.css";
+import { Route } from 'react-router-dom';
+import Search from './pages/Search';
 
 const App = () => (
   <Layout>
-    <Home/>
+    <Route path="/" component={Home} exact={true}/>
+    <Route path="/search" component={Search} exact={true}/>
     {/*<Search/>*/}
   </Layout>
 );
